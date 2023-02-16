dd7280d6d68a7c461918c5c5707cfe504ea5b41f
Salwa99 <s.ritage99@gmail.com>
2023-02-16 11:35:06 +0100

    render components

---
+++ src/pages/home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import CharacterList from '../components/CharacterList';
+
+function Home() {
+  return (
+    <section>
+      <CharacterList />
+    </section>
+  );
+}
+
+export default Home;
